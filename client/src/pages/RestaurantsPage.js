f7e428323283b612ea85b382ae703744da918deb
zaiba19 <zaibaiqbal1@gmail.com>
2019-11-27 16:10:42 -0500

    tried to display list of restaurants

--- client/src/pages/RestaurantsPage.js
+++ client/src/pages/RestaurantsPage.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-function RestaurantsPage(props) {
-  return (
-    <div>Lists of Restaurants</div>
-  );
+class RestaurantsPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      restaurants: {}
+    }
+  }
+
+  componentDidMount = () => {
+    this.getListOfRestaurants();
+  }
+
+  getListOfRestaurants = () => {
+    fetch('/api/places')
+    .then(res => res.json())
+    .then(res => this.setState({
+      restaurants: res
+    }));
+  }
+
+  
+
+  getRandomRestaurant = () => {
+    
+  }
+
+  render() {
+    return (
+      <div>Lists of Restaurants
+        <button onClick={this.getListOfRestaurants}>Get random restaurant!</button>
+      </div>
+      
+    )
+  }
 }
 
 export default RestaurantsPage;
